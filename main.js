b46110785c3565e4286eae08140811c233c5c743
PablogRizzo <pablog.rizzo@gmail.com>
2021-11-23 14:37:26 -0300

    E

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var content = document.querySelector(".content");
 var icon = document.querySelector(".fas")
 
 hamburguer.addEventListener("click", (e)=> {
+    console.log("hola");
     content.classList.toggle("activo");
     icon.classList.toggle("fa-times")
 });
